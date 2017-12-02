33f790c58cfdd8a9455e5b30e53f1a3b7b0a6016
KevinReaves <kevin82593@hotmail.com>
2017-12-01 21:03:32 -0600

    Managed to pull an event from the current date from public API, XML parsing is a bit rough

--- Twitter Bot/twitterBot.py
+++ Twitter Bot/twitterBot.py
@@ -1,10 +1,34 @@
 #!/usr/bin/python
+"""
+Thanks to Hiztory.org for the free api
+http://www.hiztory.org/
+"""
 
-import tweepy
+import tweepy, requests
+from datetime import datetime
 from credentials import *
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-api.update_status('Hello World! Testing Tweepy setup.')
+# api.update_status('Hello World! Testing Tweepy setup.')
+
+def requestHistory(date):
+    baseURL = "http://api.hiztory.org/"
+
+    curDate = date.split('-')
+
+    #For some reason I thought I needed year
+    #year = curDate[0]
+    month = curDate[1]
+    day = curDate[2]
+
+    url = baseURL + "date/event/" + month + "/" + day + "/api.xml"
+
+    history = requests.get(url, verify=False)
+    newHistory = history.text.split("\"")
+
+    print("On " + month + "/" + day + ", " + newHistory[9] + ".")
+
+requestHistory(str(datetime.now().date()))
