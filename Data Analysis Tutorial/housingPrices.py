46d43095b808b82ab8668ac47d4b74aa80958fe0
KevinReaves <kevin82593@hotmail.com>
2017-12-06 19:30:01 -0600

    Done with tutorials, moving on to project

--- Data Analysis Tutorial/housingPrices.py
+++ Data Analysis Tutorial/housingPrices.py
@@ -9,11 +9,21 @@ style.use("fivethirtyeight")
 from credentials import *
 
 
+def mortgage_30y():
+    df = quandl.get("FMAC/MORTG", trim_start="1975-01-01", authtoken = api_key)
+    df["Value"] = (df["Value"] - df["Value"][0]) / df["Value"][0] * 100.0
+
+    # correction from video game from youtube comment
+    df = df.resample('1D').mean()
+    df = df.resample('M').mean()
+    df.columns = ['M30']
+
+    return df
+
 def state_list():
     states = pd.read_html('https://en.wikipedia.org/wiki/List_of'
                           '_states_and_territories_of_the_United_States')
     return states[0][1][2:]
-
 
 def grab_initial_state_data():
     states = state_list()
@@ -42,61 +52,66 @@ def grab_initial_state_data():
 
     main_df.to_pickle("states.pickle")
 
-
 def HPI_Benchmark():
     df = quandl.get("FMAC/HPI_USA", authtoken=api_key)
     df["Value"] = (df["Value"] - df["Value"][0]) / df["Value"][0] * 100.0
 
     return df
 
-
-# default version, pandas version looks better
-# grab_initial_state_data()
-# pickle_in = open("states.pickle", "rb")
-# HPI_data = pickle.load(pickle_in)
-
-
-# grab_initial_state_data()
-
-HPI_data = pd.read_pickle("states.pickle")
-
-# Graphs HPI data against national average
-# benchmark = HPI_Benchmark()
-
-# fig = plt.figure()
-# ax1 = plt.subplot2grid((1,1),(0,0))
-#
-# HPI_data.plot(ax = ax1)
-# benchmark.plot(ax = ax1, color="k", linewidth=10)
-# plt.legend().remove()
-# plt.show()
-
-# Correlation, shows min/max/mean/std
-# HPI_State_Correlation = HPI_data.corr()
-# print(HPI_State_Correlation.describe())
-
-fig = plt.figure()
-ax1 = plt.subplot2grid((2, 1), (0, 0))
-ax2 = plt.subplot2grid((2, 1), (1, 0), sharex=ax1)
-
-# moving average for a year
-HPI_data['AL12MA'] = pd.rolling_mean(HPI_data['AL'], 12)
-HPI_data['AL12STD'] = pd.rolling_std(HPI_data['AL'], 12)
-
-#HPI_data.dropna(inplace=True)
+    # default version, pandas version looks better
+    # grab_initial_state_data()
+    # pickle_in = open("states.pickle", "rb")
+    # HPI_data = pickle.load(pickle_in)
 
 
-# HPI_data[['AL', 'AL12MA']].plot(ax=ax1)
-# HPI_data[['AL12STD']].plot(ax=ax2)
+    # grab_initial_state_data()
 
-AL_AK_12corr = pd.rolling_corr(HPI_data['AL'], HPI_data['AK'], 12)
+    #HPI_data = pd.read_pickle("states.pickle")
 
-HPI_data['AL'].plot(ax=ax1, label='AL HPI')
-HPI_data['AK'].plot(ax=ax1, label='AK HPI')
+    # Graphs HPI data against national average
+    # benchmark = HPI_Benchmark()
 
-AL_AK_12corr.plot(ax=ax2, label="AL_AK_12corr")
+    # fig = plt.figure()
+    # ax1 = plt.subplot2grid((1,1),(0,0))
+    #
+    # HPI_data.plot(ax = ax1)
+    # benchmark.plot(ax = ax1, color="k", linewidth=10)
+    # plt.legend().remove()
+    # plt.show()
 
-plt.legend(loc=4)
-plt.show()
+    # Correlation, shows min/max/mean/std
+    # HPI_State_Correlation = HPI_data.corr()
+    # print(HPI_State_Correlation.describe())
 
-gimme=input("")
+    # fig = plt.figure()
+    # ax1 = plt.subplot2grid((2, 1), (0, 0))
+    # ax2 = plt.subplot2grid((2, 1), (1, 0), sharex=ax1)
+
+    # moving average for a year
+    # HPI_data['AL12MA'] = pd.rolling_mean(HPI_data['AL'], 12)
+    # HPI_data['AL12STD'] = pd.rolling_std(HPI_data['AL'], 12)
+
+    # HPI_data.dropna(inplace=True)
+
+
+    # HPI_data[['AL', 'AL12MA']].plot(ax=ax1)
+    # HPI_data[['AL12STD']].plot(ax=ax2)
+
+    # AL_AK_12corr = pd.rolling_corr(HPI_data['AL'], HPI_data['AK'], 12)
+    #
+    # HPI_data['AL'].plot(ax=ax1, label='AL HPI')
+    # HPI_data['AK'].plot(ax=ax1, label='AK HPI')
+    #
+    # AL_AK_12corr.plot(ax=ax2, label="AL_AK_12corr")
+    #
+    # plt.legend(loc=4)
+    # plt.show()
+
+HPI_data = pd.read_pickle('states.pickle')
+m30 = mortgage_30y()
+HPI_Bench = HPI_Benchmark()
+HPI = HPI_Bench.join(m30)
+
+state_HPI_M30 = HPI_data.join(m30)
+
+print(state_HPI_M30.corr()['M30'].describe())
