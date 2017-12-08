b9de3feb49544416a8400c6b345aeef6775c293b
KevinReaves <kevin82593@hotmail.com>
2017-12-07 21:01:23 -0600

    Finished analysis of US representation data

--- US Representative Data/representation.py
+++ US Representative Data/representation.py
@@ -11,6 +11,7 @@ population : votes ratio.
 
 from credentials import *
 import pandas as pd
+import numpy as np
 import quandl
 
 
@@ -20,8 +21,6 @@ def importRepNumbers():
 
     reps = pd.read_html(line)
     df = reps[0]
-
-
 
     # dropping total row and NaN row from table
     # It looks like pandas uses a linked list in the backend, drop 51 then 52
@@ -36,6 +35,7 @@ def importRepNumbers():
     df.set_index("State", inplace=True)
     return df
 
+
 def importPopulation():
     line = 'http://www.nationsonline.org/oneworld/US-states-population.htm'
 
@@ -45,8 +45,8 @@ def importPopulation():
 
     df = pd.DataFrame(
         {
-            'Population':pop,
-            "State":state
+            'Population': pop,
+            "State": state
         }
     )
 
@@ -55,7 +55,32 @@ def importPopulation():
 
 
 reps = importRepNumbers()
+reps = reps.apply(pd.to_numeric)
 population = importPopulation()
+population = population.apply(pd.to_numeric)
 
 joined = reps.join(population)
+
+# PPR = population per representative
+joined["PPR"] = joined['Population'] // joined['Reps']
+
+# Constitution is the number of reps needed for the original 33k number
+joined["Constitution"] = joined["Population"] // 33000
+
+joined = joined.apply(np.round)
 print(joined)
+
+total = joined["Constitution"].sum()
+print("In order to meet the constitution's original goal of one rep per 33000 "
+      "citizens, we'd need " + str(total) + " reps.")
+
+lowest = np.argmin(joined["PPR"])
+highest = np.argmax(joined["PPR"])
+
+print(
+    lowest + " has the best population to representative ratio at one "
+             "representative per " + str(joined.loc[lowest]["PPR"]) + " people.")
+
+print(
+    highest + " has the worst population to representative ratio at one "
+             "representative per " + str(joined.loc[highest]["PPR"]) + " people.")
