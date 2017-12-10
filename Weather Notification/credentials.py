1c32d4fd11e6a613b4229f41b3db91464e34090e
KevinReaves <kevin82593@hotmail.com>
2017-12-10 13:15:06 -0600

    Updating blank twilio credentials

--- Weather Notification/credentials.py
+++ Weather Notification/credentials.py
@@ -4,3 +4,9 @@ openWeatherMap = ""
 #openweathermap is very picky about location. Please go to their site and find
 #what your city is called. Troy Alabama is called Troy US
 location = ""
+
+# www.twilio.com
+accountSID = ""
+authToken = ""
+twilioNumber = ""
+cellPhone = ""
