c10a501635d37ebe8bc36911d496793261d905c2
KevinReaves <kevin82593@hotmail.com>
2017-12-09 22:54:03 -0600

    Pulling weather data, plan to text or email data

--- Weather Notification/weather.py
+++ Weather Notification/weather.py
@@ -1,8 +1,31 @@
-from weather import Weather
+import pyowm
+from credentials import *
 
-weather = Weather()
+def pullWeather():
+    owm = pyowm.OWM(openWeatherMap)
 
-lookup = weather.lookup(36081)
-condition = lookup.condition()
+    observation = owm.weather_at_place(location)
+    weather = observation.get_weather()
+    return weather
 
-print(condition.text())
+
+def printWeather(weather):
+    wind = weather.get_wind()
+    temperature = weather.get_temperature('fahrenheit')
+
+    temp = str(temperature['temp'])
+    tempMin = str(temperature['temp_min'])
+    tempMax = str(temperature['temp_max'])
+    status = weather.get_status().lower()
+    windSpeed = str(wind['speed'])
+
+    returnStr = "The temperature in " + location + " is around " + temp +  \
+    " with a variation from " + tempMin + " to " + tempMax + ". \n" +  \
+    "The status is currently " + status + " and the wind speed is " + windSpeed\
+    + " miles per hour."
+
+
+    return returnStr
+
+weather = pullWeather()
+print(printWeather(weather))
