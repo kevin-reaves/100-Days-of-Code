0c25b9258276ec7944a5bdb92e690f24e7a3d6b1
Kevin Reaves <kevin82593@hotmail.com>
2018-02-01 21:07:52 -0600

    wolfram, still work in progress

---
+++ wolfram/wolfram.py
@@ -0,0 +1,30 @@
+from credentials import *
+import wolframalpha
+import xml.etree.ElementTree as ET
+
+client = wolframalpha.Client(app_id)
+
+#print(app_id)
+#question = input("Ask Wolfram Alpha a question ")
+question = "who is the us president"
+if question:
+    result = client.query(question)
+else:
+    question = input("Please try that again ")
+
+for item in result:
+    print(item)
+##root = ET.fromstring(result)
+##error = root.get("error")
+##success = root.get("success")
+##numpods = root.get("numpods")
+##answer = ""
+##
+##print(success, numpods)
+##if success and int(numpods) > 0:
+##    for plaintext in root.iter("plaintext"):
+##        answer += plaintext.text
+##    print(answer)
+##elif error:
+##    print("That query threw an error")
+##
