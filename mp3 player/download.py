6aa35675bacab880c2cb27f6ff1ab260a6738768
Kevin Reaves <kevin82593@hotmail.com>
2018-03-01 21:04:19 -0600

    downloads youtube videos, will feed into player

---
+++ mp3 player/download.py
@@ -0,0 +1,19 @@
+#import youtube_dl
+#very basic version. Downloads full video
+"""
+ydl_opts = {}
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=lqURPBtGJzg'])
+"""
+
+import pafy
+
+url = 'https://www.youtube.com/watch?v=lqURPBtGJzg'
+video = pafy.new(url)
+print("Title: " + video.title)
+print("Author: " + video.author)
+print("Duration: " + video.duration)
+
+best = video.getbest(preftype="mp4", quiet=True)
+best.download()
